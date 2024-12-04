e193bce715a312deda5a04e6fcdf677d90563b33
cgombau <cgombaute@gmail.com>
2024-12-04 23:41:50 +0100

    Multiplicar1

--- git_initprueba.java
+++ git_initprueba.java
@@ -24,6 +24,11 @@ public class git_initprueba {
 			case 2:
 				break;
 			case 3:
+				System.out.print("Ingresa un úmero entero: ");
+				num1=sc.nextInt();
+				System.out.print("Ingresa otro número entero: ");
+				num2=sc.nextInt();
+				total= num1 * num2;
 				break;
 			case 4:
 				break;
