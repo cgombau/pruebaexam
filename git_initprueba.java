400c9f23eb4eb9c4efefcb96965f26c5aceb2525
cgombau <cgombaute@gmail.com>
2024-12-04 23:43:54 +0100

    Division1

--- git_initprueba.java
+++ git_initprueba.java
@@ -26,6 +26,11 @@ public class git_initprueba {
 			case 3:
 				break;
 			case 4:
+				System.out.print("Ingresa un úmero entero: ");
+				num1=sc.nextInt();
+				System.out.print("Ingresa otro número entero: ");
+				num2=sc.nextInt();
+				total= num1 / num2;
 				break;
 			case 5:
 				System.out.print("Saliendo...");
